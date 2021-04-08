53dd5719e85930b879f60e7b89bdd9f46d51df11
pamungkas andono <pamungkas.org@gmail.com>
2021-04-08 17:27:09 +0700

    initial commit

--- app/src/main/java/com/example/android2mysharedpreferences/UserPreference.kt
+++ app/src/main/java/com/example/android2mysharedpreferences/UserPreference.kt
@@ -1,5 +1,6 @@
 package com.example.android2mysharedpreferences
 
+import android.annotation.SuppressLint
 import android.content.Context
 
 internal class UserPreference(context: Context) {
@@ -10,5 +11,29 @@ internal class UserPreference(context: Context) {
         private const val AGE = "AGE"
         private const val PHONE_NUMBER = "PHONE"
         private const val LOVE_MU = "islove"
+    }
+
+    private val preference = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+    @SuppressLint("CommitPrefEdits")
+    fun setUser(value: UserModel) {
+        val editor = preference.edit().apply {
+            putString(NAME, value.name)
+            putString(EMAIL, value.email)
+            putInt(AGE, value.age)
+            putString(PHONE_NUMBER, value.phoneNumber)
+            putBoolean(LOVE_MU, value.isLove)
+            this.apply()
+        }
+    }
+
+    fun getUser(): UserModel {
+        return UserModel().apply {
+            name = preference.getString(NAME, "")
+            email = preference.getString(EMAIL, "")
+            age = preference.getInt(AGE, 0)
+            phoneNumber = preference.getString(PHONE_NUMBER, "")
+            isLove = preference.getBoolean(LOVE_MU, false)
+        }
     }
 }
---
+++ app/src/main/java/com/example/android2mysharedpreferences/form/FormUserPreferenceActivity.kt
@@ -0,0 +1,160 @@
+package com.example.android2mysharedpreferences.form
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.text.TextUtils.isEmpty
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import com.example.android2mysharedpreferences.R
+import com.example.android2mysharedpreferences.UserModel
+import com.example.android2mysharedpreferences.UserPreference
+import com.example.android2mysharedpreferences.databinding.ActivityFormUserPreferenceBinding
+
+class FormUserPreferenceActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        const val EXTRA_TYPE_FORM = "extra_type_form"
+        const val EXTRA_RESULT = "extra_result"
+        const val RESULT_CODE = 101
+        const val TYPE_ADD = 1
+        const val TYPE_EDIT = 2
+        private const val FIELD_REQUIRED = "Field tidak boleh kosong"
+        private const val FIELD_DIGIT_ONLY = "Hanya boleh terisi numerik"
+        private const val FIELD_IS_NOT_VALID = "Email tidak valid"
+    }
+
+    private lateinit var userModel: UserModel
+
+    private lateinit var binding: ActivityFormUserPreferenceBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFormUserPreferenceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSave.setOnClickListener(this)
+
+        userModel = intent.getParcelableExtra<UserModel>("USER") as UserModel
+        val formType = intent.getIntExtra(EXTRA_TYPE_FORM, 0)
+
+        var actionBarTitle = ""
+        var btnTitle = ""
+
+        when (formType) {
+            TYPE_ADD -> {
+                actionBarTitle = "Tambah Baru"
+                btnTitle = "Simpan"
+            }
+            TYPE_EDIT -> {
+                actionBarTitle = "Ubah"
+                btnTitle = "Update"
+                showPreferenceForm()
+            }
+        }
+        supportActionBar?.title = actionBarTitle
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.btnSave.text = btnTitle
+    }
+
+    private fun showPreferenceForm() {
+        with(binding) {
+            edtName.setText(userModel.name)
+            edtEmail.setText(userModel.email)
+            edtAge.setText(userModel.age.toString())
+            edtPhone.setText(userModel.phoneNumber)
+            if (userModel.isLove) {
+                binding.rbYes.isChecked = true
+            } else {
+                binding.rbNo.isChecked = true
+            }
+        }
+    }
+
+    override fun onClick(v: View?) {
+        // TODO: 08/04/2021 tinggal menambahkan onClick event pada tiap tombol
+        if (v?.id == R.id.btn_save) {
+            val name = binding.edtName.text.toString().trim()
+            val email = binding.edtEmail.text.toString().trim()
+            val age = binding.edtAge.text.toString().trim()
+            val phoneNo = binding.edtPhone.text.toString().trim()
+            val isLoveMU = binding.rgLoveMu.checkedRadioButtonId == R.id.rb_yes
+
+            if (name.isEmpty()) {
+                binding.edtName.error = FIELD_REQUIRED
+                binding.edtName.requestFocus()
+                return
+            }
+            if (email.isEmpty()) {
+                binding.edtEmail.error = FIELD_REQUIRED
+                binding.edtEmail.requestFocus()
+                return
+            }
+            if (!isValidEmail(email)) {
+                binding.edtEmail.error = FIELD_IS_NOT_VALID
+                binding.edtEmail.requestFocus()
+                return
+            }
+
+            if (age.isEmpty()) {
+                binding.edtAge.error = FIELD_REQUIRED
+                binding.edtAge.requestFocus()
+                return
+            }
+            if (phoneNo.isEmpty()) {
+                binding.edtPhone.error = FIELD_REQUIRED
+                binding.edtPhone.requestFocus()
+                return
+            }
+            if (!TextUtils.isDigitsOnly(phoneNo)) {
+                binding.edtPhone.error = FIELD_DIGIT_ONLY
+                binding.edtPhone.requestFocus()
+                return
+            }
+
+            saveUser(name, email, age, phoneNo, isLoveMU)
+
+            val resultIntent = Intent()
+            resultIntent.putExtra(EXTRA_RESULT, userModel)
+            setResult(RESULT_CODE, resultIntent)
+
+            finish()
+        }
+    }
+
+    private fun saveUser(
+        name: String,
+        email: String,
+        age: String,
+        phoneNo: String,
+        loveMU: Boolean
+    ) {
+        val userPref = UserPreference(this)
+
+        userModel.name = name
+        userModel.email = email
+        userModel.age = age.toInt()
+        userModel.phoneNumber = phoneNo
+        userModel.isLove = loveMU
+
+        userPref.setUser(userModel)
+        Toast.makeText(this, "Data Tersimpan", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun isValidEmail(email: CharSequence): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+}
