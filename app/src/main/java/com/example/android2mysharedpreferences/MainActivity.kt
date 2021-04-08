12c5930ebc9048ddc614eee65822fdb78c24d0c3
pamungkas andono <pamungkas.org@gmail.com>
2021-04-08 18:18:11 +0700

    done main

--- app/src/main/java/com/example/android2mysharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/android2mysharedpreferences/MainActivity.kt
@@ -1,11 +1,106 @@
 package com.example.android2mysharedpreferences
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.android2mysharedpreferences.databinding.ActivityMainBinding
+import com.example.android2mysharedpreferences.form.FormUserPreferenceActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mUserPreference: UserPreference
+    private lateinit var userModel: UserModel
+
+    private var isPreferenceEmpty = false
+
+    companion object {
+        private const val REQUEST_CODE = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "My User Preference"
+
+        mUserPreference = UserPreference(this)
+
+        showExistingPreference()
+
+        binding.btnSave.setOnClickListener(this)
+    }
+
+    private fun showExistingPreference() {
+        userModel = mUserPreference.getUser()
+        populateView(userModel)
+        checkForm(userModel)
+    }
+
+    private fun populateView(userModel: UserModel) {
+        binding.tvName.text =
+            if (userModel.name.toString().isEmpty()) "Tidak Ada" else userModel.name
+
+        binding.tvEmail.text =
+            if (userModel.email.toString().isEmpty()) "Tidak Ada" else userModel.email
+
+        binding.tvAge.text =
+            if (userModel.age.toString().isEmpty()) "Tidak Ada" else userModel.age.toString()
+
+        binding.tvPhone.text =
+            if (userModel.phoneNumber.toString().isEmpty()) "Tidak Ada" else userModel.phoneNumber
+
+        binding.tvIsLoveMu.text =
+            if (userModel.isLove) "Ya" else "Tidak"
+    }
+
+    private fun checkForm(userModel: UserModel) {
+        when {
+            userModel.name.toString().isNotEmpty() -> {
+                binding.btnSave.text = getString(R.string.change)
+                isPreferenceEmpty = false
+            }
+            else -> {
+                binding.btnSave.text = getString(R.string.save)
+                isPreferenceEmpty = true
+            }
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_save) {
+            val intent = Intent(this@MainActivity, FormUserPreferenceActivity::class.java)
+            when {
+                isPreferenceEmpty -> {
+                    intent.putExtra(
+                        FormUserPreferenceActivity.EXTRA_TYPE_FORM,
+                        FormUserPreferenceActivity.TYPE_ADD
+                    )
+                    intent.putExtra("USER", userModel)
+                }
+                else -> {
+                    // TODO: 08/04/2021 melanjutkan intent jika false
+                    intent.putExtra(
+                        FormUserPreferenceActivity.EXTRA_TYPE_FORM,
+                        FormUserPreferenceActivity.TYPE_EDIT
+                    )
+                    intent.putExtra("USER", userModel)
+                }
+            }
+            startActivityForResult(intent, REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == FormUserPreferenceActivity.RESULT_CODE) {
+                userModel =
+                    data?.getParcelableExtra<UserModel>(FormUserPreferenceActivity.EXTRA_RESULT) as UserModel
+                populateView(userModel)
+                checkForm(userModel)
+            }
+        }
     }
 }
